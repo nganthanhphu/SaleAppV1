37dbb6d0750b6e278890e7bf95efe1802f41d73e
Ngàn Thành Phú <nhocjoon123@gmail.com>
2025-10-30 01:05:27 +0700

    refactor: use template inheritance

--- saleapp/index.py
+++ saleapp/index.py
@@ -5,18 +5,19 @@ from dao import load_categories, load_products, load_product_by_id
 
 app = Flask(__name__)
 
+cates = load_categories()
+
 @app.route('/')
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("cate_id")
-    cates = load_categories()
     products = load_products(q, cate_id)
     return render_template("index.html", cates = cates, products = products)
 
 @app.route('/product/<int:id>')
 def detail(id):
     product = load_product_by_id(id)
-    return render_template("product_detail.html", product = product)
+    return render_template("product_detail.html", cates = cates, product = product)
 
 if __name__ == '__main__':
     with app.app_context():
